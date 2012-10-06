1045ddc2a20ee1b0a1c701cd1465904d5173801d
Darby <darbs@HuevosRancheros.(none)>
2012-10-06 12:35:49 -0500

    added process text files assigns ratings to movies and calculates average rating

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-process through given files to produce cache docs
-average user rating 3.7
+Process through given files to produce cache docs
+Average user rating 3.7
 """
 import sys
 
@@ -12,18 +12,29 @@ totalRatings = 0.0
 avgRating = 0.0
 training = open('mv_0002043.txt', 'r')
 movies = open('movie_titles.txt','r')
+users = open('defUsers.txt','w')
+ratings = open('defRatings.txt', 'w')
 
+"""
+Reads the training data file and assigns the given rating as
+the default rating for the given users. Then calculates the average
+rating for training movie for use in the average movie rating.
+Outputs to defUsers.txt. Might think about inorporating year into this
+later
+"""
 for line in training:
-  if line.find(':') != -1:
-    numOfMovies += 1
-    print line.strip()
-  else:
+  if line.find(':') == -1:
     numOfRatings += 1
     row = [x.strip() for x in line.split(',')]
+    users.write(row[0]+","+row[1]+"\n")
     totalRatings += int(row[1])  
 avgRating = totalRatings/numOfRatings
-print avgRating
 
+
+"""
+Reads the movie data file and writes the average rating calculated
+previously as the default rating for all movies. Outpust to defRatings.txt.
+"""
 for line in movies:
   row = [x.strip() for x in line.split(',')]
-  print row[0] + " "+ str(3.77)
+  ratings.write(row[0]+","+str(avgRating)+","+row[1]+"\n")
