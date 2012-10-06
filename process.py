4695561c74445c0cd2577436188769250d809fd4
Darby <darbs@HuevosRancheros.(none)>
2012-10-05 20:17:52 -0500

    currently just computes the average rating for movie 2043 from mv_0002043.txt Processes through movies list and prints out the movie id with 3.77 as rating interestingly the calculated average rating for movie 2043 was 3.77    would be a good starting point for the ratings

--- process.py
+++ process.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 """
 process through given files to produce cache docs
+average user rating 3.7
 """
 import sys
 
-userAvgRatings = {};
+userAvgRatings = {}
+numOfMovies = 0
+numOfRatings = 0
+totalRatings = 0.0
+avgRating = 0.0
+training = open('mv_0002043.txt', 'r')
+movies = open('movie_titles.txt','r')
 
-f = open('mv_0002043.txt', 'r')
-for line in f:
+for line in training:
   if line.find(':') != -1:
-    print line
+    numOfMovies += 1
+    print line.strip()
   else:
-    print [x.strip() for x in line.split(',')]
+    numOfRatings += 1
+    row = [x.strip() for x in line.split(',')]
+    totalRatings += int(row[1])  
+avgRating = totalRatings/numOfRatings
+print avgRating
+
+for line in movies:
+  row = [x.strip() for x in line.split(',')]
+  print row[0] + " "+ str(3.77)
