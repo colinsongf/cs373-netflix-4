4388abc52166b9ded233dc224248564c45dc25cb
Darby <darbs@HuevosRancheros.(none)>
2012-10-10 07:55:51 -0500

    Now predicts for input (probe.txt) and computes RMSE against probeAnswers.txt. Run Time: ~10sec Current RMSE: 1.0615

--- RMSE.py
+++ RMSE.py
@@ -5,6 +5,13 @@
 # Copyright (C) 2012
 # Darby Perez & Newman Willis
 # ---------------------------
+"""
+Reads input file that is the result of Netflix.py and compares to probeAnswers.txt
+Then calculates RMSE.
+
+to run:
+  python runRMSE.py < probeOut.txt 
+"""
 import math
 import sys
 ratings = []
@@ -15,8 +22,7 @@ def sqre_diff (x, y) :
 def ReadOutput(results):
   for line in results:
     if line.find(':') == -1:
-      #print line[:-1].strip()
-      ratings.append(line[:-1].strip())
+      ratings.append(float(line[:-1].strip()))
   print str(RMSE(open("probeAnswers.txt"),ratings))
 
 def RMSE(probeAns, ratings):
@@ -24,9 +30,7 @@ def RMSE(probeAns, ratings):
   total = 0.0
   i =0
   for line in probeAns:
-    print line
     if line.find(':') == -1:
-      print line[:-1]
       total += sqre_diff(ratings[i],float(line[:-1]))
       i+=1
   s = len(ratings)
